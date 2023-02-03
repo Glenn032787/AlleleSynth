dfb8bd3376053cae7e9c2efb206bf9012b6e43c3
Glenn Chang <glchang@bcgsc.ca>
2023-02-03 14:20:18 -0800

    Allow multiple sample name in arg

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sampleName=$1
+# Get all argument and put it in correct format
+sampleName=$(echo $@ | tr -s '[:blank:]' ',')
+sampleName="[${sampleName}]"
 
 eval "$(conda shell.bash hook)"
 conda activate snakemake 
 
-snakemake -c 72 --use-singularity --singularity-args "-B /projects,/home,/gsc" --rerun-incomplete --restart-times=3 --config sample=$sampleName
+snakemake -c 72 --use-singularity --singularity-args "-B /projects,/home,/gsc" --rerun-incomplete --restart-times=3 --config sample=$sampleName 
